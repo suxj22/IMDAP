68ea3a69529c2eb0a32a054a92d6fe49069e73de
suxj22 <116412601+suxj22@users.noreply.github.com>
2023-09-03 17:37:57 +0800

    初始文件上传2

---
+++ build-IMDAP-Qt_6_5_1_for_macOS-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles/IMDAP_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/IMDAP_autogen.dir/ParseCache.txt"
+  "IMDAP_autogen"
+  )
+endif()
