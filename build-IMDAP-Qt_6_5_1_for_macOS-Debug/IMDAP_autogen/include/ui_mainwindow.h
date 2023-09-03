74091d3af19da66157ac9e0d4e43e91ffb520e3d
suxj22 <116412601+suxj22@users.noreply.github.com>
2023-09-03 19:36:51 +0800

    修改窗口大小

--- build-IMDAP-Qt_6_5_1_for_macOS-Debug/IMDAP_autogen/include/ui_mainwindow.h
+++ build-IMDAP-Qt_6_5_1_for_macOS-Debug/IMDAP_autogen/include/ui_mainwindow.h
@@ -15,7 +15,6 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
-#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -26,13 +25,7 @@ class Ui_MainWindow
 public:
     QAction *actionhaha;
     QWidget *centralwidget;
-    QPushButton *pushButton;
-    QPushButton *pushButton_2;
-    QPushButton *pushButton_3;
-    QPushButton *pushButton_4;
-    QPushButton *pushButton_5;
     QMenuBar *menubar;
-    QMenu *menuwow;
     QMenu *menu;
     QStatusBar *statusbar;
 
@@ -40,32 +33,15 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(800, 600);
+        MainWindow->resize(1920, 1080);
         actionhaha = new QAction(MainWindow);
         actionhaha->setObjectName("actionhaha");
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
-        pushButton = new QPushButton(centralwidget);
-        pushButton->setObjectName("pushButton");
-        pushButton->setGeometry(QRect(100, 210, 80, 18));
-        pushButton_2 = new QPushButton(centralwidget);
-        pushButton_2->setObjectName("pushButton_2");
-        pushButton_2->setGeometry(QRect(220, 200, 80, 18));
-        pushButton_3 = new QPushButton(centralwidget);
-        pushButton_3->setObjectName("pushButton_3");
-        pushButton_3->setGeometry(QRect(270, 90, 100, 32));
-        pushButton_4 = new QPushButton(centralwidget);
-        pushButton_4->setObjectName("pushButton_4");
-        pushButton_4->setGeometry(QRect(440, 130, 100, 32));
-        pushButton_5 = new QPushButton(centralwidget);
-        pushButton_5->setObjectName("pushButton_5");
-        pushButton_5->setGeometry(QRect(190, 310, 101, 61));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 800, 17));
-        menuwow = new QMenu(menubar);
-        menuwow->setObjectName("menuwow");
+        menubar->setGeometry(QRect(0, 0, 1920, 24));
         menu = new QMenu(menubar);
         menu->setObjectName("menu");
         MainWindow->setMenuBar(menubar);
@@ -73,9 +49,7 @@ public:
         statusbar->setObjectName("statusbar");
         MainWindow->setStatusBar(statusbar);
 
-        menubar->addAction(menuwow->menuAction());
         menubar->addAction(menu->menuAction());
-        menuwow->addAction(actionhaha);
 
         retranslateUi(MainWindow);
 
@@ -86,12 +60,6 @@ public:
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
         actionhaha->setText(QCoreApplication::translate("MainWindow", "haha", nullptr));
-        pushButton->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
-        pushButton_2->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
-        pushButton_3->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
-        pushButton_4->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
-        pushButton_5->setText(QCoreApplication::translate("MainWindow", "\344\270\255\346\226\207\346\214\211\351\222\256", nullptr));
-        menuwow->setTitle(QCoreApplication::translate("MainWindow", "wow", nullptr));
         menu->setTitle(QCoreApplication::translate("MainWindow", "\344\275\240\345\245\275", nullptr));
     } // retranslateUi
 
